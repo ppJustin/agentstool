b114776549374ce6e10f49251be0ef74b61c4707
ppJustin <1076093538@qq.com>
2026-03-31 12:46:41 +0800

    测试

--- demo_mini.py
+++ demo_mini.py
@@ -1,6 +1,32 @@
-#这是一个使用agents库的测试代码，主要进行env文件的测试以及部分功能的使用测试
-from hello_agents import SimpleAgent,HelloAgentsLLM
-import os 
+
+from hello_agents import SimpleAgent, HelloAgentsLLM
 from dotenv import load_dotenv
 
+# 加载环境变量
 load_dotenv()
+
+# 无指定自动检测provider，从环境变量中寻找
+llm = HelloAgentsLLM()
+
+
+
+# 创建SimpleAgent
+agent = SimpleAgent(
+    name="AI助手",
+    llm=llm,
+    system_prompt="你是一个有用的AI助手"
+)
+
+# 对话
+response = agent.run("你好！请介绍一下自己")
+print(response)
+
+# 工具模块
+from hello_agents.tools import CalculatorTool
+calculator = CalculatorTool()
+
+
+print(response)
+
+# 查看对话历史
+print(f"历史消息数: {len(agent.get_history())}")
